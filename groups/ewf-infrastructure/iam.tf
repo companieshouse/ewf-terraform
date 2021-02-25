7c3e2be3004e781136fa81e163e8f5f9e50d70fb
Marty Fox <martin.fox@kainos.com>
2021-02-25 11:55:08 +0000

    Adding correct cloudwatch log permissions for log capture from frontends

--- groups/ewf-infrastructure/iam.tf
+++ groups/ewf-infrastructure/iam.tf
@@ -1,9 +1,12 @@
 module "ewf_frontend_profile" {
   source = "git@github.com:companieshouse/terraform-modules//aws/instance_profile?ref=tags/1.0.40"
 
-  name              = "ewf_frontend_profile"
-  enable_SSM        = true
-  cw_log_group_arns = [aws_cloudwatch_log_group.ewf_fe.arn]
+  name       = "ewf_frontend_profile"
+  enable_SSM = true
+  cw_log_group_arns = [
+    "${aws_cloudwatch_log_group.ewf_fe.arn}:*",
+    "${aws_cloudwatch_log_group.ewf_fe.arn}:*:*",
+  ]
   instance_asg_arns = [module.frontend_asg.this_autoscaling_group_arn]
   kms_key_refs      = ["alias/${var.account}/${var.region}/ebs"]
   custom_statements = [
