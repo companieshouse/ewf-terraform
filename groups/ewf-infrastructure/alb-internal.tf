804c17cc3dfb38cf1c214a7dc74278fde956811d
Marc Ransome <mransome@companieshouse.gov.uk>
2021-08-17 14:15:04 +0100

    Add security group rules for CHS apps (GOV.UK Pay)

--- groups/ewf-infrastructure/alb-internal.tf
+++ groups/ewf-infrastructure/alb-internal.tf
@@ -12,6 +12,19 @@ module "ewf_internal_alb_security_group" {
   ingress_cidr_blocks = local.admin_cidrs
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
 
+  ingress_with_cidr_blocks = [
+    {
+      rule        = "http-80-tcp"
+      description = "CHS applications"
+      cidr_blocks = join(",", local.chs_vpc_subnets)
+    },
+    {
+      rule        = "https-443-tcp"
+      description = "CHS applications"
+      cidr_blocks = join(",", local.chs_vpc_subnets)
+    }
+  ]
+
   # This is a non-production ruleset, Forgerock ID Gateway access in Dev and Staging
   # When Forgerock goes into Live then the condition can be removed.
   ingress_with_source_security_group_id = var.environment == "live" ? [] : [
