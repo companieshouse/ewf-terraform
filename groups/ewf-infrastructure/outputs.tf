342fdfbb4343472ccfb51babcb308aeb96365762
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2022-07-11 16:40:29 +0100

    Added Route53 resource to create DNS entry for BEP access. Added to output also

--- groups/ewf-infrastructure/outputs.tf
+++ groups/ewf-infrastructure/outputs.tf
@@ -2,6 +2,10 @@ output "ewf_frontend_address_internal" {
   value = aws_route53_record.ewf_alb_internal.fqdn
 }
 
+output "ewf_backend_address_internal" {
+  value = aws_route53_record.ewf_bep_alb_internal.fqdn
+}
+
 output "rds_address" {
   value = aws_route53_record.ewf_rds.fqdn
 }
--- groups/ewf-infrastructure/route53.tf
+++ groups/ewf-infrastructure/route53.tf
@@ -10,6 +10,18 @@ resource "aws_route53_record" "ewf_alb_internal" {
   }
 }
 
+resource "aws_route53_record" "ewf_bep_alb_internal" {
+  zone_id = data.aws_route53_zone.private_zone.zone_id
+  name    = "${var.application}-bep"
+  type    = "A"
+
+  alias {
+    name                   = module.ewf_bep_internal_alb.this_lb_dns_name
+    zone_id                = module.ewf_bep_internal_alb.this_lb_zone_id
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_route53_record" "ewf_rds" {
   zone_id = data.aws_route53_zone.private_zone.zone_id
   name    = "${var.application}db"
