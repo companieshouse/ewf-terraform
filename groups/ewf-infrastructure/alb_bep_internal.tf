3cb4e09cfaa2df247835e693da16f8c6a8dec5c5
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2022-07-13 13:36:15 +0100

    Removed commented alarm code as not required for BEP

--- groups/ewf-infrastructure/alb_bep_internal.tf
+++ groups/ewf-infrastructure/alb_bep_internal.tf
@@ -87,31 +87,3 @@ module "ewf_bep_internal_alb" {
     )
   )
 }
-
-#--------------------------------------------
-# Internal ALB CloudWatch Alarms
-#--------------------------------------------
-#module "ewf_bep_internal_alb_alarms" {
-#  source = "git@github.com:companieshouse/terraform-modules//aws/alb-cloudwatch-alarms?ref=tags/1.0.116"
-#
-#  alb_arn_suffix            = module.ewf_bep_internal_alb.this_lb_arn_suffix
-#  target_group_arn_suffixes = module.ewf_bep_internal_alb.target_group_arn_suffixes
-#
-#  prefix                    = "ewf-internal-bep-"
-#  response_time_threshold   = "100"
-#  evaluation_periods        = "3"
-#  statistic_period          = "60"
-#  maximum_4xx_threshold     = "2"
-#  maximum_5xx_threshold     = "2"
-#  unhealthy_hosts_threshold = "1"
-#
-#  # If actions are used then all alarms will have these applied, do not add any actions which you only want to be used for specific alarms
-#  # The module has lifecycle hooks to ignore changes via the AWS Console so in this use case the alarm can be modified there.
-#  actions_alarm = var.enable_sns_topic ? [module.cloudwatch_sns_notifications[0].sns_topic_arn] : []
-#  actions_ok    = var.enable_sns_topic ? [module.cloudwatch_sns_notifications[0].sns_topic_arn] : []
-#
-#  depends_on = [
-#    module.cloudwatch_sns_notifications,
-#    module.ewf_bep_internal_alb
-#  ]
-#}
